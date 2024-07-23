cad09505cd654feb3c3f44c02db61b6d93a15c1b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-23 21:11:39 +0530

    Update LandingImage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/LandingImage.jsx
+++ client/src/components/HomePage/LandingImage.jsx
@@ -7,14 +7,21 @@ const LandingImage = () => {
             <div className="md:grid grid-cols-2 gap-4">
                 <div className=""></div>
                 <div className="py-40 md:mr-24 md:ml-4 md:mx-0 mx-4 ">
-                    <div className="bg-white py-4 px-8 rounded">
-                        <div className="text-center text-xl font-semibold text-gray-500">Select Train</div>
+                    <div className="">
+                        <div className="md:flex my-4 justify-between md:mx-16 mx-4">
+                            <button className="md:my-0 my-2 bg-none text-white py-4 px-4 border-2 border-white w-full mx-2 rounded duration-500 hover:bg-white hover:text-blue-500">Search Train</button>
+                            <button className="md:my-0 my-2 bg-none text-white py-4 px-4 border-2 border-white w-full mx-2 rounded duration-500 hover:bg-white hover:text-blue-500">Login</button>
+                            <button className="md:my-0 my-2 bg-none text-white py-4 px-4 border-2 border-white w-full mx-2 rounded duration-500 hover:bg-white hover:text-blue-500">Registation</button>
+                        </div>
+                    </div>
+                    <div className="bg-white py-4 px-8 rounded md:h-[400px]">
+                        <div className="text-center text-xl font-semibold text-gray-500 mt-8 mb-6">Select Train</div>
                         <hr />
 
                         <div className="">
                             <form method="post">
                                 <div className="md:grid grid-cols-2 gap-4 mt-4">
-                                    <div className="">
+                                    <div className="md:my-0 my-2">
                                         <select name="" id="" className='w-full h-12 bg-gray-200 rounded pl-4' required>
                                             <option value="">To</option>
                                             <option value="">Colombo</option>
@@ -24,10 +31,10 @@ const LandingImage = () => {
                                             <option value="">Colombo</option>
                                         </select>
                                     </div>
-                                    <div className="">
+                                    <div className="md:my-0 my-2">
                                         <input type="date" name="" id="" className='w-full h-12 bg-gray-200 rounded pl-4' required/>
                                     </div>
-                                    <div className="">
+                                    <div className="md:my-0 my-2">
                                         <select name="" id="" className='w-full h-12 bg-gray-200 rounded pl-4' required>
                                             <option value="">From</option>
                                             <option value="">Colombo</option>
@@ -37,6 +44,19 @@ const LandingImage = () => {
                                             <option value="">Colombo</option>
                                         </select>
                                     </div>
+                                    <div className="md:my-0 my-2">
+                                        <select name="" id="" className='w-full h-12 bg-gray-200 rounded pl-4' required>
+                                            <option value="">Class</option>
+                                            <option value="">Colombo</option>
+                                            <option value="">Colombo</option>
+                                            <option value="">Colombo</option>
+                                            <option value="">Colombo</option>
+                                            <option value="">Colombo</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div className="my-8">
+                                    <button className="bg-blue-500 text-white py-4 px-8 rounded w-full">Search Train</button>
                                 </div>
                             </form>
                         </div>
