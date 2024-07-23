0854d138bde1395c7a508be3cf863ca9fbc5a523
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-23 20:54:50 +0530

    Update LandingImage.jsx
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/LandingImage.jsx
+++ client/src/components/HomePage/LandingImage.jsx
@@ -3,8 +3,13 @@ import React from 'react'
 const LandingImage = () => {
   return (
     <div className='mt-[-60px] bg-[url(https://wallpapercave.com/wp/wp5682288.jpg)] bg-cover bg-center min-h-screen'>
-        <div className="pt-5">
-            Train Bookngs
+        <div className="">
+            <div className="md:grid grid-cols-2 gap-4">
+                <div className=""></div>
+                <div className="">
+                    Ho alll
+                </div>
+            </div>
         </div>
     </div>
   )
