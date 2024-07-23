a62fcc9dc55e1a8e2179e40be36f423e5fdbd316
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-23 20:43:56 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- client/src/components/HomePage/LandingImage.jsx
+++ client/src/components/HomePage/LandingImage.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const LandingImage = () => {
   return (
-    <div>LandingImage</div>
+    <div className='bg-[url(https://wallpapercave.com/wp/wp5682288.jpg)] bg-cover bg-center min-h-screen'>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magni illum est porro sequi, ducimus libero perspiciatis unde veritatis perferendis accusantium minus vero obcaecati laudantium, quas, deleniti praesentium nesciunt molestias rem?
+    </div>
   )
 }
 
