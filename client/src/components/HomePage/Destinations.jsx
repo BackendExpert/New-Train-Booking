745acaeab6b1b005d4c58ccd4bf50cdf42cc7c88
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-07-23 16:56:18 +0530

    Update Destinations.jsx

--- client/src/components/HomePage/Destinations.jsx
+++ client/src/components/HomePage/Destinations.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Destinations = () => {
   return (
-    <div>Destinations</div>
+    <div className='md:my-20 my-12 md:mx-40 mx-8'>
+        <h1 className="text-2xl text-center text-gray-500 font-semibold">Destinations</h1>
+    </div>
   )
 }
 
